de691c19d67e24aca72c0b12c078d8e5ff8aeb53
LouisePietropaoli <louise.pietropaoli@gmail.com>
2021-03-03 09:43:11 +0100

    master - error endif

---
+++ CMakeFiles/e-chess.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/nikitaterekhov/CLionProjects/e-chess/CMakeFiles/3.17.3/CompilerIdCXX/CMakeCXXCompilerId.cpp" "/Users/nikitaterekhov/CLionProjects/e-chess/CMakeFiles/e-chess.dir/CMakeFiles/3.17.3/CompilerIdCXX/CMakeCXXCompilerId.cpp.o"
+  "/Users/nikitaterekhov/CLionProjects/e-chess/src/controller/GameController.cpp" "/Users/nikitaterekhov/CLionProjects/e-chess/CMakeFiles/e-chess.dir/src/controller/GameController.cpp.o"
+  "/Users/nikitaterekhov/CLionProjects/e-chess/src/main.cpp" "/Users/nikitaterekhov/CLionProjects/e-chess/CMakeFiles/e-chess.dir/src/main.cpp.o"
+  "/Users/nikitaterekhov/CLionProjects/e-chess/src/model/Bishop.cpp" "/Users/nikitaterekhov/CLionProjects/e-chess/CMakeFiles/e-chess.dir/src/model/Bishop.cpp.o"
+  "/Users/nikitaterekhov/CLionProjects/e-chess/src/model/Chessboard.cpp" "/Users/nikitaterekhov/CLionProjects/e-chess/CMakeFiles/e-chess.dir/src/model/Chessboard.cpp.o"
+  "/Users/nikitaterekhov/CLionProjects/e-chess/src/model/King.cpp" "/Users/nikitaterekhov/CLionProjects/e-chess/CMakeFiles/e-chess.dir/src/model/King.cpp.o"
+  "/Users/nikitaterekhov/CLionProjects/e-chess/src/model/Knight.cpp" "/Users/nikitaterekhov/CLionProjects/e-chess/CMakeFiles/e-chess.dir/src/model/Knight.cpp.o"
+  "/Users/nikitaterekhov/CLionProjects/e-chess/src/model/Pawn.cpp" "/Users/nikitaterekhov/CLionProjects/e-chess/CMakeFiles/e-chess.dir/src/model/Pawn.cpp.o"
+  "/Users/nikitaterekhov/CLionProjects/e-chess/src/model/Piece.cpp" "/Users/nikitaterekhov/CLionProjects/e-chess/CMakeFiles/e-chess.dir/src/model/Piece.cpp.o"
+  "/Users/nikitaterekhov/CLionProjects/e-chess/src/model/Queen.cpp" "/Users/nikitaterekhov/CLionProjects/e-chess/CMakeFiles/e-chess.dir/src/model/Queen.cpp.o"
+  "/Users/nikitaterekhov/CLionProjects/e-chess/src/model/Tower.cpp" "/Users/nikitaterekhov/CLionProjects/e-chess/CMakeFiles/e-chess.dir/src/model/Tower.cpp.o"
+  "/Users/nikitaterekhov/CLionProjects/e-chess/src/view/Button.cpp" "/Users/nikitaterekhov/CLionProjects/e-chess/CMakeFiles/e-chess.dir/src/view/Button.cpp.o"
+  "/Users/nikitaterekhov/CLionProjects/e-chess/src/view/ClickableElement.cpp" "/Users/nikitaterekhov/CLionProjects/e-chess/CMakeFiles/e-chess.dir/src/view/ClickableElement.cpp.o"
+  "/Users/nikitaterekhov/CLionProjects/e-chess/src/view/GraphicElement.cpp" "/Users/nikitaterekhov/CLionProjects/e-chess/CMakeFiles/e-chess.dir/src/view/GraphicElement.cpp.o"
+  "/Users/nikitaterekhov/CLionProjects/e-chess/src/view/Point2I.cpp" "/Users/nikitaterekhov/CLionProjects/e-chess/CMakeFiles/e-chess.dir/src/view/Point2I.cpp.o"
+  "/Users/nikitaterekhov/CLionProjects/e-chess/src/view/View.cpp" "/Users/nikitaterekhov/CLionProjects/e-chess/CMakeFiles/e-chess.dir/src/view/View.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/e-chess.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/e-chess.dir/CMakeFiles/3.17.3/CompilerIdCXX/CMakeCXXCompilerId.cpp.o"
+  "CMakeFiles/e-chess.dir/src/controller/GameController.cpp.o"
+  "CMakeFiles/e-chess.dir/src/main.cpp.o"
+  "CMakeFiles/e-chess.dir/src/model/Bishop.cpp.o"
+  "CMakeFiles/e-chess.dir/src/model/Chessboard.cpp.o"
+  "CMakeFiles/e-chess.dir/src/model/King.cpp.o"
+  "CMakeFiles/e-chess.dir/src/model/Knight.cpp.o"
+  "CMakeFiles/e-chess.dir/src/model/Pawn.cpp.o"
+  "CMakeFiles/e-chess.dir/src/model/Piece.cpp.o"
+  "CMakeFiles/e-chess.dir/src/model/Queen.cpp.o"
+  "CMakeFiles/e-chess.dir/src/model/Tower.cpp.o"
+  "CMakeFiles/e-chess.dir/src/view/Button.cpp.o"
+  "CMakeFiles/e-chess.dir/src/view/ClickableElement.cpp.o"
+  "CMakeFiles/e-chess.dir/src/view/GraphicElement.cpp.o"
+  "CMakeFiles/e-chess.dir/src/view/Point2I.cpp.o"
+  "CMakeFiles/e-chess.dir/src/view/View.cpp.o"
+  "build/e-chess"
+  "build/e-chess.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/e-chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
